0cdb9ebca99a1ecb45f9f8115366bed50d703de7
khadar-oothuru <khadaroothuru@gmail.com>
2025-06-30 21:05:21 +0530

    Resolved login css issue

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './Login.css';
+import './login.css';
 import loginImage from '../assets/image.png';
 
 const Login = () => {
